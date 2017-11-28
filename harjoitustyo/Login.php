495b2daf066f3f0683c802cbefe13714eb2fd044
severi <severikupari1@gmail.com>
2017-11-28 10:56:28 +0200

    lisatty kenttia asiakastauluun

--- harjoitustyo/Login.php
+++ harjoitustyo/Login.php
@@ -6,14 +6,15 @@
 </head>
 <body>
     
-    <form action="Login-check.php" method="get">
-        
-    Nimi : <input type="text" name="nimi" value="">
-    Käyntiosoite:    <input type="text" name="kayntiosoite" value="">
-    Laskutusosoite:    <input type="text" name="laskutusosoite" value="">
-    Puhelinnumero:    <input type="text" name="puhelinnumero" value="">
-    Sähköposti:    <input type="text" name="email" value="">
-        <input type="submit" value="" name="rekisteroi">
+    <form action="Login-check.php" method="get"> <br>
+    Käyttäjätunnus: <input type="text" name="kayttajatunnus" value="">    <br>
+    Salasana : <input type="text" name="salasana" value=""> <br>
+    Nimi : <input type="text" name="nimi" value=""> <br>
+    Käyntiosoite:    <input type="text" name="kayntiosoite" value=""> <br>
+    Laskutusosoite:    <input type="text" name="laskutusosoite" value=""> <br>
+    Puhelinnumero:    <input type="text" name="puhelinnumero" value=""> <br>
+    Sähköposti:    <input type="text" name="email" value=""> <br>
+        <input type="submit" value="rekisteröi" name="rekisteroi"> <br>
     </form>
     
     
