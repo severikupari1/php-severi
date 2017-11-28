9daffd2182ec73958a08b2ebd7a938e9795985d5
severi <severikupari1@gmail.com>
2017-11-28 10:37:22 +0200

    harjoitustyo

---
+++ harjoitustyo/Login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Kirjautuminen</title>
+</head>
+<body>
+    
+    <form action="Login-check.php" method="get">
+        
+    Nimi : <input type="text" name="nimi" value="">
+    Käyntiosoite:    <input type="text" name="kayntiosoite" value="">
+    Laskutusosoite:    <input type="text" name="laskutusosoite" value="">
+    Puhelinnumero:    <input type="text" name="puhelinnumero" value="">
+    Sähköposti:    <input type="text" name="email" value="">
+        <input type="submit" value="" name="rekisteroi">
+    </form>
+    
+    
+</body>
+</html>
